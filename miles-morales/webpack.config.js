3337f8a2830f7fa92b0b32705c3dfe33ed2f99e6
manuhon99 <manuhon99@gmail.com>
2021-02-18 14:52:54 -0300

    adding flask app

---
+++ miles-morales/webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader'],
+      },
+    ],
+  },
+ 
+};
